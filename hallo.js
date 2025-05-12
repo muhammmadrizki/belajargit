65ddaf014156eecc1ae9e10e53c370210153b8ca
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-05-12 14:44:09 +0700

    changing in js

--- hallo.js
+++ hallo.js
@@ -1,7 +1,4 @@
-console.log("Hello Worldd");
 let number = 75;
 if (number > 75) {
   console.log("You passed the test");
-} else {
-  console.log("belajar lagi");
 }
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("halo dunia");
+console.info("Ini informasi");
